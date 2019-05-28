bab6cf79601c58c5d8ddb85ee3f1023a3d9ee89f
denisafonov <desafono@gmail.com>
2019-05-29 00:33:38 +0300

    1.3. JavaDoc. Code convention[#142102]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ *Class Calculate Задача 1.1. Создание Hello World [#142100]
+ *@author Denis
+ *@since 21.05.2019
+ *@version 1
+ */
+ 
+ /**
+ *Main - метод для вывода на консоль Not Hello World.
+ *@ arg - arg.
+ */
+ 
 public class Calculate {
 	
 	public static void main(String[] args) {
--- src/ru/job4j/calculate/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for calculate task.
-*
-* @author Denis Safonov (desafono@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j.calculate;
