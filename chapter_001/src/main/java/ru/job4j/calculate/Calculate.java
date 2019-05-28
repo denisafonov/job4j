2944ec8130d6e0b091e556981c51c7266ff711d1
denisafonov <desafono@gmail.com>
2019-05-29 01:27:27 +0300

    1.4. JUnit[#142103]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,22 +1,29 @@
 package ru.job4j.calculate;
 
 /**
- *Class Calculate Задача 1.1. Создание Hello World [#142100]
- *@author Denis
- *@since 21.05.2019
- *@version 1
- */
- 
- /**
- *Main - метод для вывода на консоль Not Hello World.
- *@param args - arg.
+ * Calculate.
+ *
+ * @author Denis Safonov (desafono@gmail.com)
+ * @version $Id$
+ * @since 0.1
  */
  
 public class Calculate {
-	
+	/**
+	*Main - метод для вывода на консоль Not Hello World.
+	*@param args - arg.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Not Hello World");
 	
 	}
-
+	
+	/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
