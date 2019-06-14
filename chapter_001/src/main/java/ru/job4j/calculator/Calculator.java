02fc1b1b17d8fd62fe4cc1407b2a0b8d0c16e788
denisafonov <desafono@gmail.com>
2019-06-14 20:06:01 +0300

    Javadoc

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,16 +1,47 @@
 package ru.job4j.calculator;
 
-public class Calculator {
+/**
+ * Calculator.
+ *
+ * @author Denis Safonov (desafono@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 
+public class Calculator {
+    /**
+     * Add - метод сложения.
+     * @param first - первая переменная
+     * @param second - вторая переменная
+     * @return - итог
+     */
     public double add(double first, double second) {
         return first + second;
     }
+    /**
+     * Subtract - метод вычитания.
+     * @param first - первая переменная
+     * @param second - вторая переменная
+     * @return - итог
+     */
     public double subtract(double first, double second) {
         return first - second;
     }
+    /**
+     * Divide - метод деления.
+     * @param first - первая переменная
+     * @param second - вторая переменная
+     * @return - итог
+     */
     public double div(double first, double second) {
         return first / second;
     }
+    /**
+     * Multiple - метод умножения.
+     * @param first - первая переменная
+     * @param second - вторая переменная
+     * @return - итог
+     */
     public double miltiple(double first, double second){
         return first * second;
     }
--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -4,7 +4,18 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test.
+ *
+ * @author Denis Safonov (desafono@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class CalculatorTest {
+    /**
+     * Test add.
+     */
     @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -12,7 +23,9 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
-
+    /**
+     * Test subtract.
+     */
     @Test
     public void whenSub2On2Then1() {
         Calculator calc = new Calculator();
@@ -20,7 +33,9 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
-
+    /**
+     * Test divide.
+     */
     @Test
     public void whenDiv2On2Then1() {
         Calculator calc = new Calculator();
@@ -28,7 +43,9 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
-
+    /**
+     * Test multiple.
+     */
     @Test
     public void whenMul2On2Then1() {
         Calculator calc = new Calculator();
